4e6ac03a4ee5cd6f26b74aa4852c966e7c584bfe
Anderson-Soares-Martins <andersoaresmartins@gmail.com>
2024-10-02 15:18:59 -0300

    feat: adicionado a opção de baixar o curriculo

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useRef, useState } from "react";
-import { ChevronRight } from "lucide-react";
+import { ChevronRight, FileDown, X } from "lucide-react";
 
 import MobileMenu from "@/components/MobileMenu";
 import { Profile } from "./components/Profile";
@@ -11,6 +11,9 @@ import { Education } from "./components/Education";
 import { Certifications } from "./components/Certifications";
 import { SoftSkills } from "./components/SoftSkills";
 import { Languages } from "./components/Languages";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import PulsatingButton from "@/components/ui/pulsating-button";
 
 const menuItems = [
   { id: "profile", label: "Profile" },
@@ -25,6 +28,7 @@ const menuItems = [
 const AboutMe = () => {
   const [activeSection, setActiveSection] = useState("profile");
   const sectionRefs = useRef<{ [key: string]: HTMLElement | null }>({});
+  const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
     const observerOptions = {
@@ -119,9 +123,9 @@ const AboutMe = () => {
 
   return (
     <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
-      <aside className="fixed top-14 z-30 -ml-2 hidden flex-1 w-full shrink-0 md:sticky md:block">
+      <aside className="fixed top-14 z-10 -ml-2 hidden flex-1 w-full shrink-0 md:sticky md:block">
         <div className="h-full py-6 pr-6 lg:py-8">
-          <div className="h-full rounded-md  bg-background">
+          <div className="h-full rounded-md">
             <div className="p-4">
               <h4 className="mb-1 rounded-md px-2 py-1 text-sm font-semibold">
                 Getting Started
@@ -165,6 +169,66 @@ const AboutMe = () => {
           </div>
         </div>
       </main>
+
+      <PulsatingButton
+        className="fixed  bottom-4 right-4"
+        onClick={() => setOpenModal(true)}
+      >
+        <FileDown /> CV
+      </PulsatingButton>
+
+      {openModal && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div
+            className="absolute inset-0"
+            onClick={() => setOpenModal(false)}
+          />
+          <div className="relative bg-background rounded-lg p-8 max-w-[32rem] w-full">
+            <Button
+              className="absolute top-4 right-4"
+              variant={"ghost"}
+              onClick={() => setOpenModal(false)}
+            >
+              <X />
+            </Button>
+            <h2 className="text-3xl font-semibold text-center">
+              Download Resume
+            </h2>
+            <div className="flex justify-center mt-8 space-x-10">
+              <Link
+                href={"/Linknderson Soares Martins - Resume.pdf"}
+                target="_blank"
+                download={"Anderson Soares Martins - Resume.pdf"}
+                rel="noopener noreferrer"
+              >
+                <Button
+                  variant="outline"
+                  className="gap-x-2"
+                  onClick={() => setOpenModal(false)}
+                >
+                  <FileDown />
+                  English
+                </Button>
+              </Link>
+              <Link
+                href={"/Anderson Soares Martins - Currículo.pdf"}
+                target="_blank"
+                download={"Anderson Soares Martins - Currículo.pdf"}
+                rel="noopener noreferrer"
+              >
+                <Button
+                  variant="outline"
+                  className="gap-x-2"
+                  onClick={() => setOpenModal(false)}
+                >
+                  <FileDown />
+                  Português
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
