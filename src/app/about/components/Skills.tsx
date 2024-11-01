f1530b6f0badf224793e97f407b1f6c19a87b44a
Anderson-Soares-Martins <andersoaresmartins@gmail.com>
2024-11-01 08:56:59 -0300

    update

--- src/app/about/components/Skills.tsx
+++ src/app/about/components/Skills.tsx
@@ -13,12 +13,14 @@ export const Skills = forwardRef<
         </h1>
       </div>
       <ul className="list-disc pl-6 space-y-2">
-        <li>Languages: JavaScript, TypeScript</li>
+        <li>Languages: JavaScript, TypeScript, SQL</li>
         <li>
           Frameworks and Libraries: React, Next.js, React Native, Tailwind CSS,
           Styled-components
         </li>
-        <li>Tools and Platforms: Git, GitHub, Docker, Firebase, Vercel</li>
+        <li>
+          Tools and Platforms: Git, GitHub, Docker, Firebase, Vercel, Postgre
+        </li>
         <li>Methodologies: Agile, Scrum</li>
         <li>
           Other Skills: UX/UI Design, Unit Testing (Jest, Testing Library),
